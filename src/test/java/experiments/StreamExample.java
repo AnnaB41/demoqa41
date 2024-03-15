187fbac9357423b7e454b8dda011824922304d5f
AnnaB41 <159689092+AnnaB41@users.noreply.github.com>
2024-03-15 20:41:47 +0200

    Allure, Lambda, Stream

---
+++ src/test/java/experiments/StreamExample.java
@@ -0,0 +1,34 @@
+package experiments;
+
+import java.util.*;
+
+
+public class StreamExample { ;
+
+
+            public void mapWithStream() {
+            Map<String, Integer> map = new HashMap<>();
+            map.put("A", 1);
+            map.put("B", 2);
+            map.put("C", 3);
+
+            // Using stream to print the map
+            map.entrySet().stream()
+                    .forEach(entry -> System.out.println
+                            (entry.getKey() + ": " + entry.getValue()));
+        }
+
+        public void mapWithoutStream() {
+            Map<String, Integer> map = new HashMap<>();
+            map.put("A", 1);
+            map.put("B", 2);
+            map.put("C", 3);
+
+            // Without using stream
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
+    }
+
+
