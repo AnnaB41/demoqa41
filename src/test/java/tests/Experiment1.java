e9183758a21ec2a48f36a9af6cf3db199815148d
AnnaB41 <159689092+AnnaB41@users.noreply.github.com>
2024-04-24 21:58:16 +0300

    Rectangler and Iframes

--- src/test/java/tests/Experiment1.java
+++ src/test/java/tests/Experiment1.java
@@ -1,15 +1,18 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Set;
 
 public class Experiment1 {
@@ -64,9 +67,94 @@ public class Experiment1 {
     }
 
     @Test
-    public void testRectangle(){
+    public void testRectangle() throws InterruptedException {
 
-    //TODO
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://demoqa.com/slider");
+        driver.manage().window().maximize();
+
+        WebElement range = driver.findElement(By.xpath("//input[@type='range']"));
+
+        Rectangle rectangleRange = range.getRect(); // получить прямоугольник элемента
+        int widthRange = rectangleRange.width;  // получить ширину прямоугольника
+        System.out.println("with = "+ widthRange);
+
+        Actions actions = new Actions(driver);
+
+        actions.moveToElement(range).perform();  // попадаем на левый верхний угол
+
+        int xLocation = range.getLocation().getX();  // получаем нашу локацию по оси х
+        System.out.println("x location " + xLocation);
+
+      //  int xTo = (widthRange - xLocation)/2;  // вычисляем, насколько нам нужно подвинуть
+      int xTo2 = (widthRange)/3;  // вычисляем, насколько нам нужно подвинуть
+
+
+        actions.moveByOffset(xTo2, 0).click().perform();  // показываем, куда передвинуть точку
+
+        Thread.sleep(5000);
+
+        driver.quit();
+
+
+
+       }
+
+       @Test
+       public void iframeTest() throws InterruptedException {
+            WebDriver driver = new FirefoxDriver();
+            driver.get("https://the-internet.herokuapp.com/iframe");
+           driver.manage().window().maximize();
+
+           Thread.sleep(2000);
+
+           By textElementInIframe = By.xpath("//body[@id='tinymce']/p"); // локатор для элемента
+
+
+           List<WebElement> beforeIframe = driver.findElements(textElementInIframe); // т.к. элемент находится
+           // внутри айфрейма, то возвращается 0 элементов
+           System.out.println("before switch to iframe should be zero " + beforeIframe.size());
+
+         //  driver.switchTo().frame(0); //переключились на айфрейм, 0 это означает 1 айфрейм, как в массивах
+           driver.switchTo().frame("mce_0_ifr"); // переключаемся на айфрейм по id
+
+           List<WebElement> insideIframe = driver.findElements(textElementInIframe);
+           System.out.println("after switch to iframe should be one " + insideIframe.size());
+
+           driver.switchTo().defaultContent(); // ушли из айфрейм
+
+           List<WebElement> backToDefault = driver.findElements(textElementInIframe);
+           System.out.println("after switch back to default should be one " + backToDefault.size());
+
+           driver.quit();
+       }
+
+       @Test
+       public void iframeNestedTest() throws InterruptedException {
+
+           WebDriver driver = new FirefoxDriver();
+           driver.get("https://the-internet.herokuapp.com/nested_frames");
+           driver.manage().window().maximize();
+
+           Thread.sleep(2000);
+
+           By bodyTag = By.xpath("//body");
+           System.out.println("not a frame, size 0: " + driver.findElements(bodyTag).size());
+
+           driver.switchTo().frame("frame-top");
+           driver.switchTo().frame(0);
+           System.out.println("LEFT + " + driver.findElement(bodyTag).getText());
+
+           driver.switchTo().defaultContent(); // ушли из айфрейм
+           driver.switchTo().defaultContent(); // ушли из айфрейм
+
+
+           driver.switchTo().frame("frame-bottom");
+           System.out.println("bottom + " + driver.findElement(bodyTag).getText());
+
+           driver.quit();
+
+
 
        }
 
